6d8590efa77ec39428953b838ee95fcfcf56a24a
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-09 12:44:04 -0400

    weird tests

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  // call functions here
+
+  submitForm();
 });
 
-// define functions here
+function submitForm(){
+  $('form').on('submit', function(event){
+    var listItem = $('#item').val();
+    $('ol').append( '<li>' + listItem + '</li>' );
+    event.preventDefault();
+  });
+}
